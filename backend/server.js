2ebbda3a1412c69167804b086528bb65debbee10
dcx <dcx2607@gmail.com>
2025-06-11 15:03:39 +0100

    this commit serves the frontend fromm the backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const voteRoutes = require('./route/voteRoute');
 
 const app = express()
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'Frontend')));
 app.use(cors())
 const authRoutes = require('./route/signUp')
 app.use('/api', voteRoutes);
