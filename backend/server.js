26ce525048f7fbca6a0fc6d186bae104aba3c005
dcx2024 <166218735+dcx2024@users.noreply.github.com>
2025-06-11 15:09:00 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const voteRoutes = require('./route/voteRoute');
 
 const app = express()
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '..', 'Frontend')));
+app.use(express.static(path.join(__dirname)));
+
 app.use(cors())
 const authRoutes = require('./route/signUp')
 app.use('/api', voteRoutes);
