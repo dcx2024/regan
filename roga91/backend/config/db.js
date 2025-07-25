a5976e358ea2b3ca3d1caa5857366a892da492ee
dcx2024 <166218735+dcx2024@users.noreply.github.com>
2025-07-25 21:59:32 +0100

    Update db.js

--- roga91/backend/config/db.js
+++ roga91/backend/config/db.js
@@ -3,6 +3,13 @@ const path = require('path');
 
 // Load environment variables from ../.env
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+console.log('Loaded DB config:', {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+});
 
 const db = new Client({
   user: process.env.DB_USER,
